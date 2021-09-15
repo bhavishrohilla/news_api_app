a768995a8f4904849b527795aff1e1f9bfc67c1e
Bhavish Rohilla <bhavishrohilla59@gmail.com>
2021-09-15 20:21:05 +0530

    categories

--- news_api/views.py
+++ news_api/views.py
@@ -4,12 +4,19 @@ API_KEY = 'd0b69496c18e463f888a273cb521ea9f'
 # Create your views here.
 def home(request):
     country = request.GET.get('country')
-    url = f'https://newsapi.org/v2/top-headlines?country={country}&apikey={API_KEY}'
-    response = requests.get(url)
-    data = response.json()
-    print(data)
+    category = request.GET.get('category')
 
-    articles = data['articles']
+    if country:
+        url = f'https://newsapi.org/v2/top-headlines?country={country}&apikey={API_KEY}'
+        response = requests.get(url)
+        data = response.json()
+        articles = data['articles']
+    else:
+        url = f'https://newsapi.org/v2/top-headlines?category={category}&apikey={API_KEY}'
+        response = requests.get(url)
+        data = response.json()
+        articles = data['articles']
+        
     context = {
         'articles' : articles
     }
