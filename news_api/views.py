b486ae1c89773d8eeeacb3e7daa8e357274053e1
Bhavish Rohilla <bhavishrohilla59@gmail.com>
2021-09-15 19:29:23 +0530

    search by things

--- news_api/views.py
+++ news_api/views.py
@@ -3,7 +3,8 @@ import requests
 API_KEY = 'd0b69496c18e463f888a273cb521ea9f'
 # Create your views here.
 def home(request):
-    url = f'https://newsapi.org/v2/top-headlines?country=in&apikey={API_KEY}'
+    country = request.GET.get('country')
+    url = f'https://newsapi.org/v2/top-headlines?country={country}&apikey={API_KEY}'
     response = requests.get(url)
     data = response.json()
     print(data)
